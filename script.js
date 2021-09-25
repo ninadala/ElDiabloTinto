49f0efff36ad784f98081568c29791cbc5cc0f99
Nina Sellal <nina.sellal@hotmail.fr>
2021-09-25 11:55:34 +0200

    le jeu est focntionnel

--- script.js
+++ script.js
@@ -1,105 +1,124 @@
-//VARIABLES GLOBALEs
-let coups = 0;
-
-//EVENEMENTS
-let btnNewGame         = document.querySelector('#btnNewGame');
-let zonePlayer1        = document.querySelector('#zonePlayer1');
-let zonePlayer2        = document.querySelector('#zonePLayer2');
-let btnRollPlayer1     = document.querySelector('#btnRollPlayer1');
-let btnRollPlayer2     = document.querySelector('#btnRollPlayer2');
-let btnHoldPlayer1     = document.querySelector('#btnHoldPlayer1');
-let btnHoldPlayer2     = document.querySelector('#btnHoldPlayer2');
-let globalScorePlayer1 = document.querySelector('#globalScorePlayer1');
-let globalScorePlayer2 = document.querySelector('#globalScorePlayer2');
-let roundScorePlayer1  = document.querySelector('#roundScorePlayer1');
-let roundScorePlayer2  = document.querySelector('#roundScorePlayer2');
+//AJOUT DES ELEMENTS DU DOM
+let btnNewGame         = document.querySelector('#newGame');
+let btnRoll            = document.querySelector('#roll');
+let btnHold            = document.querySelector('#hold');
 let dice               = document.querySelector('#dice');
 
+let namePlayer1        = document.querySelector('#player1');
+let roundScorePlayer1  = document.querySelector('#roundScorePlayer1');
+let globalScorePlayer1 = document.querySelector('#globalScorePlayer1');
+let icon1              = document.querySelector('#icon1');
 
-//CLASSES
+let namePlayer2        = document.querySelector('#player2');
+let roundScorePlayer2  = document.querySelector('#roundScorePlayer2');
+let globalScorePlayer2 = document.querySelector('#globalScorePlayer2');
+let icon2              = document.querySelector('#icon2');
 
+//INITIALISATION DE LA CLASSE
 class Player {
-    constructor(pseudo) {
-        this.pseudo      = pseudo;
-        this.roundScore  = 0;
+    constructor(pseudo, roundScore, globalScore) {
+        this.pseudo = pseudo;
+        this.roundScore = 0;
         this.globalScore = 0;
     }
 
-    get informations() {
-        return "current" + this.globalScore;
-    }
-
-    hold() {
-        this.globalScore = this.globalScore + this.roundScore;
-        this.roundScore == 0;
-    }
-
-    win() {
+    checkWin() {
         if(this.globalScore >= 100) {
-            this.globalScore == 0;
-            console.log(this.pseudo + " a gagné la partie !");
+            alert(`${this.pseudo} a gagné la partie !`);
+            newGame();
         }
     }
-
-    roll() {
-        let min = 1;
-        let max = 7;
-        console.log(Math.floor (Math.random() * (max - min)+min));
-    }
 }
+
+
+//VARIABLES GLOBALES
+let currentPlayer = 0;
+let player1 = new Player("player 1");
+let player2 = new Player("player 2");
+icon1.style.display = 'none';
+icon2.style.display = 'none';
 
 //FONCTIONS
-
+//fonction NEW GAME
 function newGame() {
-    var player1 = new Player("player 1");
-    var player2 = new Player("player 2");
+    player1.roundScore = 0;
+    player2.roundScore = 0;
+    player1.globalScore = 0;
+    player2.globalScore = 0;
+    roundScorePlayer1.innerHTML = 0;
+    roundScorePlayer2.innerHTML = 0;
+    globalScorePlayer1.innerHTML = 0;
+    globalScorePlayer2.innerHTML = 0;
+    dice.innerHTML = 0;
+    currentPlayer = 0;
+    icon1.style.display = 'inline';
+    icon2.style.display = 'none';
 }
 
-function rollTheDice(player) {
-    let essai = player.roll();
-
-    if(essai == 1) {
-        player.roundScore = 0;
-    }
-    else {
-        player.roundScore += essai;
-        essai == 0;
-    }
+//fonction ROLL
+function roll() {
+    let min = 1;
+    let max = 7;
+    return(Math.floor (Math.random() * (max - min)+min));
 }
 
+//fonction HOLD
 function hold(player) {
     player.globalScore += player.roundScore;
-    player.roundScore == 0;
+    player.roundScore = 0;
+    dice.innerHTML = 0;
 }
 
-function nextPlayer() {
-    if(coups%2 == 0) {
-        btnRollPlayer1.style.display = 'block';
-        btnHoldPlayer1.style.display = 'block';
-        btnRollPlayer2.style.display = 'none';
-        btnHoldPlayer2.style.display = 'none';
+//DEROULÉ DU JEU
+btnNewGame.addEventListener('click', newGame);
+
+btnRoll.addEventListener('click', () => {
+    let diceRoll = roll();
+    dice.innerHTML = diceRoll;
+
+    if(currentPlayer%2 == 0) {
+        icon1.style.display = 'inline';
+        icon2.style.display = 'none'; 
+
+        if(diceRoll == 1) {
+            player1.roundScore == 0;
+            roundScorePlayer1.innerHTML = 0;
+            currentPlayer ++;
+        } else {
+            player1.roundScore += diceRoll;
+            roundScorePlayer1.innerHTML = player1.roundScore;
+            diceRoll == 0;
+        }
     }
     else {
-        btnRollPlayer1.style.display = 'none';
-        btnHoldPlayer1.style.display = 'none';
-        btnRollPlayer2.style.display = 'block';
-        btnHoldPlayer2.style.display = 'block';
+        icon2.style.display = 'inline';
+        icon1.style.display = 'none';
+
+        if(diceRoll == 1) {
+            player2.roundScore == 0;
+            roundScorePlayer2.innerHTML = 0;
+            currentPlayer ++;
+        } else {
+            player2.roundScore += diceRoll;
+            roundScorePlayer2.innerHTML = player2.roundScore;
+            diceRoll == 0;
+        }
     }
-}
+})
 
-
-//PROCHAINS TRUCS A FAIRE
-// switch pour le dice
-// faire le html
-
-
-
-//ALGORITHM
-
-// player 1 roll the dices
-// result of the dice add to ROUNDSCORE
-// choose if ROLL the dice again or if HOLD
-// if ROLL and if the result is 1 => ROUNDSCORE = 0 & change to player 2
-// if HOLD => ROUNDSCORE is add to GLOBALSCORE & change to player 2
-// if GLOBALSCORE >= 100 => player 1 is the winner
-
+btnHold.addEventListener('click', () => {
+    if(currentPlayer%2 == 0) {
+        hold(player1);
+        globalScorePlayer1.innerHTML = player1.globalScore;
+        roundScorePlayer1.innerHTML  = player1.roundScore;
+        currentPlayer ++;
+        player1.checkWin();
+    }
+    else {
+        hold(player2);
+        globalScorePlayer2.innerHTML = player2.globalScore;
+        roundScorePlayer2.innerHTML  = player2.roundScore;
+        currentPlayer ++;
+        player2.checkWin();
+    }
+})
