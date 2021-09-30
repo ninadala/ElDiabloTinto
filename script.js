7c05602f31aff77e09433588442a81d1607ef9dc
Nina Sellal <nina.sellal@hotmail.fr>
2021-09-30 10:44:28 +0200

    Try to create a "Game" class but it failed
    Revert "script.js done and clean"
    
    This reverts commit 841ce02d1b567d0f604da6da45f7507d5b47b8d9.

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-//CLASSES
+// PROCHAINE ETAPE
+// créer une classe game pour intégrer les querySelector dedans
+
+//AJOUT DES ELEMENTS DU DOM
+let btnNewGame         = document.querySelector('#newGame');
+let btnRoll            = document.querySelector('#roll');
+let btnHold            = document.querySelector('#hold');
+let dice               = document.querySelector('#dice');
+
+//INITIALISATION DE LA CLASSE
 class Player {
     constructor(pseudo, id, roundScore, globalScore) {
         this.pseudo = pseudo;
@@ -34,17 +43,8 @@ class Player {
     }
 }
 
-class Game {
-    constructor() {
-        this.btnNewGame = document.querySelector('#newGame');
-        this.btnRoll = document.querySelector('#roll');
-        this.btnHold = document.querySelector('#hold');
-        this.dice = document.querySelector('#dice');
-    }
-}
 
-
-//GLOBAL VARIABLE
+//VARIABLES GLOBALES
 let currentPlayer = null;
 let player1 = new Player("player 1", 1);
 let player2 = new Player("player 2", 2);
@@ -90,7 +90,7 @@ function permut() {
     }
 }
 
-//GAMEPLAY
+//DEROULÉ DU JEU
 btnNewGame.addEventListener('click', newGame);
 
 btnRoll.addEventListener('click', () => {
